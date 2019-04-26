44e999da9b494a6ca020df326ca591c6e0e23da2
chanuuuuu <dn0208@gmail.com>
2019-04-26 21:15:07 +0900

    (04.26) back-end 로그인 구현

---
+++ service/middlewares/checkAuthOnReq.js
@@ -0,0 +1,21 @@
+var checkAuthOnReq = function(req, res, next){
+    
+    //request 객체가 passport를 가지고 있는지 체크한다.
+    // this가 제대로 자신을 참조하고 있는지 확인해야한다.
+    req.isAuthenticated = function() {
+        var property = 'user';
+        if (this._passport && this._passport.instance._userProperty) {
+        property = this._passport.instance._userProperty;
+        }
+        
+        return (this[property]) ? true : false;
+    };
+    
+    req.isUnauthenticated = function() {
+        return !this.isAuthenticated();
+    };
+
+    next();
+}
+
+module.exports = checkAuthOnReq;
