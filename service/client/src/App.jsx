fe793b6ccf77b486939be990ccf9833de817a6a9
hwasurr <hwasurr@MacBookProui-MacBookPro.local>
2019-04-30 20:32:20 +0900

    react-router-dom 설치 및 route

--- service/client/src/App.jsx
+++ service/client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Main from './components/Main';
-
 /* App component do routing all the other components
 */
 
-const App = () => <Main />;
+const App = () => (
+  <div>
+    <Route exact path="/" component={Main} />
+  </div>
+);
 
 export default App;
