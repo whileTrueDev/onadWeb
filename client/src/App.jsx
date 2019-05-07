564b14e2bb76312b0386fc37380979c71018ff58
hwasurr <iamsupermazinga@gmail.com>
2019-05-07 21:35:48 +0900

    merged!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import RegistStepper from './components/regist/stepper';
 import Main from './components/Main';
-import RegistStepper from './regist/stepper';
 /* App component do routing all the other components */
 
 const App = () => (
--- service/app.js
+++ service/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('./passportStrategy');
-const pool = require('./connectionPool'); // local path로 key값을 사용하기 때문에 동일한 경로의 파일일 경우, 같은 객체를 공유하게 된다.
+const pool = require('./model/connectionPool'); // local path로 key값을 사용하기 때문에 동일한 경로의 파일일 경우, 같은 객체를 공유하게 된다.
 const checkLogin = require('./middlewares/ensureAuthenticated');
 
 
@@ -50,7 +50,6 @@ app.use('/', homeRouter);
 app.use('/login', loginRouter);
 app.use('/user', checkLogin, userRouter);
 app.use('/regist', registRouter); 
-app.use('/inquiry', inquiryRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
