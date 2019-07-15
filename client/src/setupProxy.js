8fbb877436274e8dacd4cc7930c0039afed50167
hwasurr <iamsupermazinga@gmail.com>
2019-07-15 17:03:27 +0900

    hostname 변경

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,15 +1,17 @@
 const proxy = require('http-proxy-middleware');
 
+const HOST = process.env.NODE_ENV === 'production' ? 'http://52.79.211.82:3000' : 'http://localhost:3000';
+
 module.exports = (app) => {
   app.use(
     proxy('/api', {
-      target: 'http://localhost:3000',
+      target: HOST,
       changeOrigin: true,
     }),
   );
   app.use(
     proxy('/images', {
-      target: 'http://localhost:3000',
+      target: HOST,
       changeOrigin: true,
     }),
   );
