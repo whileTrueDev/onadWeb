956ff6581e59e9164d8876810aef95f287cd4e5f
hwasurr <iamsupermazinga@gmail.com>
2019-07-19 17:50:46 +0900

    hostname 수정

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const HOST = process.env.NODE_ENV === 'production' ? 'http://52.79.211.82:3000' : 'http://localhost:3000';
+const HOST = process.env.NODE_ENV === 'production' ? 'https://onad.io:3000' : 'http://localhost:3000';
 
 export default HOST;
