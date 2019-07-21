ec9e13a17c050616c37d5f8bf468f3825c5f8217
hwasurr <iamsupermazinga@gmail.com>
2019-07-21 13:49:28 +0900

    hostname 변경 / http - https

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const HOST = process.env.NODE_ENV === 'production' ? 'https://onad.io:3000' : 'http://localhost:3000';
+const HOST = process.env.NODE_ENV === 'production' ? 'https://api.onad.io' : 'http://localhost:3000';
 
 export default HOST;
