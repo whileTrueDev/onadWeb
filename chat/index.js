ad5c0b6204a100bd0e7293903fec4877b5292f89
hwasurr <iamsupermazinga@gmail.com>
2019-10-31 13:47:21 +0900

    쿼리 오타 수정

--- chat/index.js
+++ chat/index.js
@@ -2,7 +2,7 @@ const tmi = require('tmi.js');
 
 const BOT_USERNAME = 'adbot';
 const OAUTH_TOKEN = 'oauth:ql78nrmxylz561jfwizzu7vi973vld';
-const CHANNEL_NAME = 'iamsupermazinga';
+const CHANNEL_NAME = 'silphtv';
 const CHANNEL_NAME2 = 'dingception';
 
 // Define configuration options
@@ -24,12 +24,6 @@ const opts = {
 const client = new tmi.Client(opts); // Create a client with our options
 client.connect(); // Connect to Twitch:
 
-// Function called when the "dice" command is issued
-function rollDice() {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
 // Date Fuction called when the message comes in
 function now() {
   const thisTime = new Date();
@@ -39,17 +33,36 @@ function now() {
 // Called every time a message comes in
 function onMessageHandler(target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  console.log(context);
-  console.log(`${target} [${now()}] <${context['display-name']}> ${msg}`);
 
+  const data = {
+    channel: target.slice(1),
+    time: now(),
+    name: context['display-name'],
+    id: context.username,
+    subscriber: context.subscriber,
+    badges: context.badges,
+    manager: context.mod,
+    text: msg
+  };
+
+  console.log(data);
+
+
+  /** *************************************
+   * 챗봇의 영역.
+   * 메시지 중, 특정 단어의 경우 대답하도록 한다.
+   * 크리에이터의 oauth 권한 허용이 필요하다.
+   **************************************** */
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+
+  // const commandName = msg.trim();
+
   // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  }
+  // if (commandName === '!dice') {
+  //   const num = rollDice();
+  //   client.say(target, `You rolled a ${num}`);
+  //   console.log(`* Executed ${commandName} command`);
+  // }
 }
 
 // Called every time the bot connects to Twitch chat
--- service/app.js
+++ service/app.js
@@ -81,7 +81,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.log(`Error occurred in - ${req.route.path}\n${err}`);
+  console.log(`[${new Date().toLocaleString()}] Error occurred in - ${req.route.path}\n${err}`);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
