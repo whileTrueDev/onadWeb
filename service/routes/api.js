6a3775e11cd22079d8ec703c2cf9d5dd12d250df
dn0208@gmail.com <dn0208@gmail.com>
2019-11-14 12:16:03 +0900

    이미지 크롤링을 위한 라우터 추가

--- service/routes/api.js
+++ service/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const doQuery = require('../model/doQuery');
 
 const router = express.Router();
 
@@ -11,5 +12,53 @@ router.use('/login', loginRouter);
 router.use('/regist', registRouter);
 router.use('/dashboard', dashboardRouter);
 router.use('/pay', payRouter);
+
+const getStreamerList = () => {
+  const currentTimeQuery = `
+  SELECT date 
+  FROM campaignTimestamp 
+  ORDER BY date DESC 
+  LIMIT 1`;
+
+  const streamerQuery = `
+  SELECT campaignId, creatorId
+  FROM campaignTimestamp
+  WHERE date > ? `;
+
+  return new Promise((resolve, reject) => {
+    doQuery(currentTimeQuery)
+      .then((row) => {
+        const { date } = row.result[0];
+        date.setMinutes(date.getMinutes() - 5);
+        return date;
+      })
+      .then((time) => {
+        doQuery(streamerQuery, [time])
+          .then((inrow) => {
+            if (!inrow.error) {
+              resolve(inrow.result);
+            } else {
+              resolve([]);
+            }
+          })
+          .catch((errorData) => {
+            errorData.point = 'getStreamerList()';
+            errorData.description = '최신 streamerId을 구하는 과정';
+            reject(errorData);
+          });
+      })
+      .catch((errorData) => {
+        errorData.point = 'getStreamerList()';
+        errorData.description = 'twitchStreamDetail의 최신 time을 구하는 과정';
+        reject(errorData);
+      });
+  });
+};
+
+
+router.get('/creators', async (req, res) => {
+  const creatorList = await getStreamerList();
+  res.send(creatorList);
+});
 
 module.exports = router;
