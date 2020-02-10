4bbc38f6334fab732c42ffd5c72aba038c3269cd
leejineun <kevin2022@naver.com>
2020-02-10 16:30:51 +0900

    시간선택/기간선택 disabled 제거

--- client/src/organisms/marketer/CampaignCreate/TimeSelectorSet.jsx
+++ client/src/organisms/marketer/CampaignCreate/TimeSelectorSet.jsx
@@ -19,7 +19,7 @@ const TimeSelectorSet = (props) => {
           <GreenCheckbox
             name="no-limit"
             checked={timeSelectorOpen === false}
-            onClick={handleTimeSelectorOpen} // 기능 구현 이후 주석 제거
+            onClick={handleTimeSelectorOpen}
             fontSize="large"
           />
           <StyledSelectText
@@ -29,8 +29,6 @@ const TimeSelectorSet = (props) => {
         </Grid>
       </Grid>
       <Grid item>
-        {/* ***************** NOTICE ***************** */}
-        {/* SOCKET 광고 송출 서버 기능 구현 이후 disabled 삭제 */}
 
         <Grid container direction="row">
           <GreenCheckbox
@@ -39,18 +37,14 @@ const TimeSelectorSet = (props) => {
             onChange={handleChange}
             onClick={handleTimeSelectorOpen}
             fontSize="large"
-            disabled // 기능 구현 이후 disabled prop 삭제
           />
           <StyledSelectText
             primary="송출 시간대 설정"
-            onClick={handleTimeSelectorOpen} // 기능 구현 이후 주석 제거
-            style={{ cursor: 'pointer' }} // 기능 구현 이후 주석 제거
-            secondary="곧 지원 예정입니다."
+            onClick={handleTimeSelectorOpen}
+            style={{ cursor: 'pointer' }}
           />
         </Grid>
 
-        {/* SOCKET 광고 송출 서버 기능 구현 이후 disabled 삭제 */}
-        {/* ***************** NOTICE ***************** */}
       </Grid>
     </Grid>
   );
