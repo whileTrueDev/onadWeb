5f0d7655326b821412a63a357d7bb2cccde197c2
dn0208@gmail.com <dn0208@gmail.com>
2020-02-26 14:57:16 +0900

    [UPDATE #5.2] marketer profile router 완료

--- server/routes/marketer/profile/index.ts
+++ server/routes/marketer/profile/index.ts
@@ -4,16 +4,10 @@ import logoutRouter from '../../auth/logout';
 import responseHelper from '../../../middlewares/responseHelper';
 import dataProcessing from '../../../lib/dataProcessing';
 import doQuery from '../../../model/doQuery';
+import encrypto from '../../../middlewares/encryption';
+
 
 const router = express.Router();
-
-// 넣을 uri
-// history
-// business-license
-// account
-// tax-bills
-// notifications
-// notification
 
 // marketer/actionLog에서 가져옴.
 router.route('/history')
@@ -39,10 +33,8 @@ router.route('/history')
             }
         }),
     )
-    .post(responseHelper.middleware.unusedMethod)
-    .put(responseHelper.middleware.unusedMethod)
-    .patch(responseHelper.middleware.unusedMethod)
-    .delete(responseHelper.middleware.unusedMethod);
+    .all(responseHelper.middleware.unusedMethod)
+
 
 
 
@@ -54,15 +46,22 @@ router.route('/account')
             const { marketerId } = responseHelper.getSessionData(req);
             if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
                 const query = `
-                SELECT *
-                FROM marketerActionLog
-                WHERE marketerId = ?
-                ORDER BY date DESC
-                LIMIT 100
-                `;
+                SELECT marketerAccountNumber, accountHolder
+                FROM marketerInfo
+                WHERE marketerId = ?`;
                 doQuery(query, [marketerId])
                     .then((row) => {
-                        responseHelper.send(row.result, 'get', res);
+                        const { marketerAccountNumber, accountHolder } = row.result[0];
+                        let accountNumber;
+                        if (marketerAccountNumber) {
+                            accountNumber = encrypto.decipher(marketerAccountNumber);
+                        } else {
+                            accountNumber = '';
+                        }
+
+                        responseHelper.send({
+                            accountNumber, accountHolder
+                        }, 'get', res);
                     })
                     .catch((error) => {
                         responseHelper.promiseError(error, next);
@@ -70,91 +69,190 @@ router.route('/account')
             }
         }),
     )
-    .post(responseHelper.middleware.unusedMethod)
-    .put(responseHelper.middleware.unusedMethod)
-    .patch(responseHelper.middleware.unusedMethod)
-    .delete(responseHelper.middleware.unusedMethod);
+    .all(responseHelper.middleware.unusedMethod)
+
+// marketer/sub/profile => /business, /business/upload 가져옴.
+router.route('/business')
+    .all(responseHelper.middleware.checkSessionExists)
+    .get(
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const { marketerId } = responseHelper.getSessionData(req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const query = `
+                SELECT marketerBusinessRegNum, marketerBusinessRegSrc
+                FROM marketerInfo
+                WHERE marketerId = ?`;
+                doQuery(query, [marketerId])
+                    .then((row) => {
+                        responseHelper.send(row.result[0], 'get', res);
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    })
+            }
+        }),
+    )
+    .put(
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const businessImageSrc = responseHelper.getParam('imageUrl', 'PUT', req);
+            const { marketerId } = responseHelper.getSessionData(req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const query = `
+                UPDATE marketerInfo
+                SET marketerBusinessRegSrc = ?
+                WHERE marketerId = ?`;
+                doQuery(query, [businessImageSrc, marketerId])
+                    .then(() => {
+                        responseHelper.send([true], 'PUT', res);
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    })
+            }
+        }),
+    )
+    .all(responseHelper.middleware.unusedMethod)
 
 
+interface Taxbill {
+    state: string | number;
+    cashAmount: string;
+}
+// marketer/sub/profile => /accountNuber에서 가져옴.
+router.route('/tax-bills')
+    .get(
+        responseHelper.middleware.checkSessionExists, // session 확인이 필요한 경우.
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const { marketerId } = responseHelper.getSessionData(req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const query = `
+                SELECT date, cashAmount, state FROM marketerTaxBill
+                WHERE marketerId = ?`;
+                doQuery(query, [marketerId])
+                    .then((row) => {
+                        const sendArray: any[] = [];
+                        row.result.forEach((obj: Taxbill) => {
+                            const object = obj;
+                            let taxBillState = '';
+                            switch (object.state) {
+                                case 0: taxBillState = '발행대기'; break;
+                                case 1: taxBillState = '발행완료'; break;
+                                case 2: taxBillState = '미발행'; break;
+                                default: throw Error('tax bill state');
+                            }
+                            object.state = taxBillState;
+                            object.cashAmount = object.cashAmount.toString();
+                            sendArray.push(Object.values(object));
+                        });
+                        responseHelper.send(sendArray, 'get', res);
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    })
+            }
+        }),
+    )
+    .all(responseHelper.middleware.unusedMethod)
 
-//   // 등록된 사업자 등록증 스캔본 조회
-//   // marketerInfo
-//   router.get('/business', (req, res) => {
-//     const marketerId = req._passport.session.user.userid;
-//     const businessRegiQuery = `
-//     SELECT marketerBusinessRegNum, marketerBusinessRegSrc
-//     FROM marketerInfo
-//     WHERE marketerId = ?`;
-//     doQuery(businessRegiQuery, [marketerId])
-//       .then((row) => {
-//         if (!row.error && row.result.length > 0) {
-//           res.send(row.result[0]);
-//         }
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         res.send({});
-//       });
-//   });
+router.route('/notification')
+    .get(
+        responseHelper.middleware.checkSessionExists, // session 확인이 필요한 경우.
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const { marketerId } = responseHelper.getSessionData(req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const callQuery = `
+                SELECT 
+                mn.index, title, content,
+                date_format(date,'%y년 %m월 %d일') AS dateform,
+                readState
+                FROM marketerNotification AS mn
+                WHERE marketerId = ?
+                ORDER BY date DESC, readState ASC`;
+
+                const countQuery = `
+                SELECT count(*) as count
+                FROM marketerNotification
+                WHERE marketerId = ? AND readState = 0`;
+                const result = { notifications: [{}], unReadCount: 0 };
+
+                doQuery(callQuery, [marketerId])
+                    .then((data) => {
+                        result.notifications = data.result;
+                        doQuery(countQuery, [marketerId])
+                            .then((row) => {
+                                if (row.result) {
+                                    const { count } = row.result[0];
+                                    result.unReadCount = count;
+                                }
+                                responseHelper.send(result, 'get', res);
+                            });
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    });
+            }
+        })
+    )
+    .patch(
+        responseHelper.middleware.checkSessionExists, // session 확인이 필요한 경우.
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const { marketerId } = responseHelper.getSessionData(req);
+            const index = responseHelper.getParam("index", "PATCH", req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const query = `
+                UPDATE marketerNotification
+                SET readState = 1
+                WHERE marketerNotification.index = ? AND marketerId = ?`;
+
+                doQuery(query, [index, marketerId])
+                    .then(() => {
+                        responseHelper.send([true], 'PATCH', res);
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    })
+            }
+        })
+    )
+    .all(responseHelper.middleware.unusedMethod)
 
 
-//   // 사업자 등록증 스캔본 업로드
-//   // marketerInfo
-//   router.post('/business/upload', (req, res) => {
-//     const marketerId = req._passport.session.user.userid;
-//     const businessRegistrationImageSrc = req.body.imageUrl;
+// marketer/sub/profile => /accountNuber에서 가져옴.
 
-//     const businessRegiQuery = `
-//     UPDATE marketerInfo
-//     SET marketerBusinessRegSrc = ?
-//     WHERE marketerId = ?`;
-//     const businessRegiArray = [businessRegistrationImageSrc, marketerId];
+interface Notification {
+    title: string;
+    content: string;
+    data: string;
+    readState: number | string;
+}
 
-//     doQuery(businessRegiQuery, businessRegiArray)
-//       .then((row) => {
-//         if (!row.error) {
-//           res.send([true, '등록되었습니다.']);
-//         }
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         res.end();
-//       });
-//   });
+router.route('/notifications')
+    .get(
+        responseHelper.middleware.checkSessionExists, // session 확인이 필요한 경우.
+        responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+            const { marketerId } = responseHelper.getSessionData(req);
+            if (responseHelper.paramValidationCheck(marketerId, 'marketerId', req)) {
+                const query = `
+                SELECT title, content, date_format(date,'%y. %m. %d') as date, readState
+                FROM marketerNotification AS mn
+                WHERE marketerId = ?
+                ORDER BY readState`;
 
-//   // 마케터 세금계산 정보 조회
-//   router.get('/taxbill', (req, res) => {
-//     const marketerId = req._passport.session.user.userid;
-
-//     const query = `
-//     SELECT date, cashAmount, state FROM marketerTaxBill
-//     WHERE marketerId = ?`;
-
-//     const queryArray = [marketerId];
-
-//     doQuery(query, queryArray).then((row) => {
-//       const sendArray = [];
-//       if (!row.error && row.result) {
-//         row.result.forEach((obj) => {
-//           const object = obj;
-
-//           let taxBillState = '';
-//           switch (object.state) {
-//             case 0: taxBillState = '발행대기'; break;
-//             case 1: taxBillState = '발행완료'; break;
-//             case 2: taxBillState = '미발행'; break;
-//             default: throw Error('tax bill state');
-//           }
-
-//           object.state = taxBillState;
-//           object.cashAmount = object.cashAmount.toString();
-//           sendArray.push(Object.values(object));
-//         });
-//         res.send(sendArray);
-//       }
-//     });
-//   });
-
-
+                doQuery(query, [marketerId])
+                    .then((data) => {
+                        const dataArray = data.result.map((value: Notification) => {
+                            const returnValue: Notification = { ...value };
+                            returnValue.readState = returnValue.readState ? '읽음' : '안읽음';
+                            return Object.values(returnValue)
+                        });
+                        responseHelper.send(dataArray, 'get', res);
+                    })
+                    .catch((error) => {
+                        responseHelper.promiseError(error, next);
+                    })
+            }
+        }),
+    )
+    .all(responseHelper.middleware.unusedMethod)
 
 export default router;
