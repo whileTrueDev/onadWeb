d9bf24e971ea0dd864f366f49ce6fdbbdd96eb35
GoMotiv <newage3333@gmail.com>
2020-02-27 19:59:14 +0900

    크리에이터 정보 조회

--- server/routes/creator/index.ts
+++ server/routes/creator/index.ts
@@ -1,8 +1,48 @@
 import express from 'express';
+import responseHelper from '../../middlewares/responseHelper';
+import dataProcessing from '../../lib/dataProcessing';
+import doQuery from '../../model/doQuery';
+import encrypto from '../../middlewares/encryption';
 
 const router = express.Router();
 
 router.route('/')
-  .get();
+  .get(
+    responseHelper.middleware.checkSessionExists,
+    responseHelper.middleware.withErrorCatch(async (req, res, next) => {
+      const session = responseHelper.getSessionData(req);
+      const { creatorId } = responseHelper.getSessionData(req);
+      const NowIp: any = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+      if (responseHelper.paramValidationCheck(creatorId, 'creatorId', req)) {
+
+        const query = `
+        SELECT creatorId, creatorName, creatorIp, creatorMail, 
+        creatorAccountNumber, creatorContractionAgreement, creatorTwitchId, realName
+        FROM creatorInfo
+        WHERE creatorId = ?
+        `;
+
+        doQuery(query, [creatorId])
+          .then(row => {
+            const userData = row.result[0];
+            const rawAccount: string = row.result[0].creatorAccountNumber || '';
+            const deciphedAccountNum: string = encrypto.decipher(rawAccount);
+            userData.creatorLogo = session.creatorLogo;
+            userData.creatorAccountNumber = deciphedAccountNum;
+            const result: object = {
+              ...userData,
+              NowIp
+            }
+            responseHelper.send(result, 'get', res);
+          }).catch((error) => {
+            responseHelper.promiseError(error, next)
+          }
+          )
+      }
+    }),
+
+  )
+  .all(responseHelper.middleware.unusedMethod);
 
 export default router;
