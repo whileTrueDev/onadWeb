2fde502d7fa1031482e166cbaf67e8a7935db18d
hwasurr <iamsupermazinga@gmail.com>
2020-03-17 10:43:14 +0900

    chatbot 옮기기위해 삭제

--- chat/index.ts
+++
@@ -1,7 +0,0 @@
-import TwitchChatCollectorV2 from './model/collector';
-
-
-// const TwitchChatCollector = require('./model/TwitchChatCollector');
-
-const onad = new TwitchChatCollectorV2();
-onad.run();
