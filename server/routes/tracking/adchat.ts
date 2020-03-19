7dff321d74bc15c1798f1ce1932bb871fccf5c51
iamsupermazinga <iamsupermazinga@gmail.com>
2020-03-20 00:11:09 +0900

    adchat 라우터 작성중

--- server/routes/tracking/adchat.ts
+++ server/routes/tracking/adchat.ts
@@ -5,51 +5,65 @@ import doQuery from '../../model/doQuery';
 
 const router = express.Router();
 
+/**
+clickedTime: 클릭 시간
+conversionTime: 전환 시간
+action: 클릭/전환 상태
+linkId: 링크의 아이디
+campaignId: 해당 캠페인 아이디
+campaignName: 해당 캠페인 아이디
+marketerId: 해당 캠페인의 마케터 아이디
+creatorId: 클릭의 출처
+creatorTwitchId: 클릭의 출처 (트위치아이디)
+ip: 접속자 IP
+device: 접속자 기기
+os: 접속자 OS
+os_version: 접속자 OS버전
+browser: 접속자 브라우저
+browser_version: 접속자 브라우저 버전
+browser_engine: 접속자 브라우저 엔진
+payout: 금액
+*/
 router.route('/')
   .post(responseHelper.middleware.withErrorCatch(async (req, res, next) => {
-    const NowIp: any = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(req.query);
+    const NowIp: string | string[] | undefined = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const [campaignId, referrer, userAgent] = responseHelper.getParam(['campaignId', 'referrer', 'userAgent'], 'post', req);
-    console.log('ip: ', NowIp);
-    console.log('campaignId: ', campaignId);
     console.log('referrer: ', referrer);
+    console.log(referrer.split('twitch.tv/'));
 
+    const creatorTwitchId = referrer.split('twitch.tv/')[1];
     const UA = new uaparser.UAParser(userAgent);
-    const device = UA.getDevice();
-    const Browser = UA.getBrowser();
+    const uaDevice = UA.getDevice();
+    const device = `${uaDevice.vendor},${uaDevice.model},${uaDevice.type}`; // null 처리 필요
+    const browser = UA.getBrowser();
     const OS = UA.getOS();
-    const Engine = UA.getEngine();
-    const CPU = UA.getCPU();
+    const browserEngine = UA.getEngine();
 
-    console.log(device, Browser, OS, Engine, CPU);
-    // { vendor: undefined, model: undefined, type: undefined }
-    // { name: 'Chrome', version: '79.0.3945.130', major: '79' }
-    // { name: 'Mac OS', version: '10.15.3' }
-    // { name: 'Blink', version: '79.0.3945.130' }
-    // { architecture: undefined }
+    const selectQuery = `
+    SELECT campaignName, linkId, links, creatorId
+      FROM campaign
+      JOIN linkRegistered ON linkId = connectedLinkId
+      RIGHT JOIN creatorInfo ON creatorTwitchId = ?
+    WHERE campaignId = ?
+    `;
+    const selectArray = [creatorTwitchId, campaignId];
 
-
-    /**
-    clickedTime: 클릭 시간
-    conversionTime: 전환 시간
-    action: 클릭/전환 상태
-    linkId: 링크의 아이디
-    campaignId: 해당 캠페인 아이디
-    campaignName: 해당 캠페인 아이디
-    marketerId: 해당 캠페인의 마케터 아이디
-    creatorId: 클릭의 출처
-    creatorTwitchId: 클릭의 출처 (트위치아이디)
-    ip: 접속자 IP
-    device: 접속자 기기
-    os: 접속자 OS
-    os_version: 접속자 OS버전
-    agent: 접속자: user-agent
-    payout: 금액
-    */
-
-    // const query = `
-
-    // `;
+    const insertQuery = `
+    INSERT INTO tracking (
+      action, conversionTime, linkId, campaignId, campaignName, marketerId,
+      creatorId, creatorTwitchId, ip, device, os, os_version,
+      browser, browser_version, browser_engine, browser_engine_version, payout
+    ) VALUES (
+      ?, ?, ?, ?, ?, ?, 
+      ?, ?, ?, ?, ?, ?, 
+      ?, ?, ?, ?
+    )`;
+    const queryArray = [
+      'CPC', null, linkId, campaignId, campaignName, campaignId.split('_')[0],
+      creatorId, creatorTwitchId, NowIp, device, OS.name, OS.version,
+      browser.name, browser.version, browserEngine.name, browserEngine.version,
+      payout
+    ];
 
     // const row = await doQuery(query, [campaignId]);
 
