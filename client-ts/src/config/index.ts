411decca0ee49dd5b01280e9a18f1baa283415c4
iamsupermazinga <iamsupermazinga@gmail.com>
2020-03-28 16:21:03 +0900

    HOST env 기반으로

--- client-ts/src/config/index.ts
+++ client-ts/src/config/index.ts
@@ -1,3 +1,4 @@
-const HOST = process.env.NODE_ENV === 'production' ? 'https://api.onad.io' : 'http://localhost:3000';
-// const HOST = 'http://localhost:3000';
+const HOST = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_API_HOSTNAME : process.env.DEV_API_HOSTNAME;
+export const REACT_HOST = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_REACT_HOSTNAME : process.env.DEV_REACT_HOSTNAME;
+
 export default HOST;
