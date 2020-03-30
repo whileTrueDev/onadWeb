8e344a78c163faba41fe23401d2a6d129a0ef60c
hwasurr <iamsupermazinga@gmail.com>
2020-03-30 19:10:31 +0900

    host명 수정 + workflow 수정

--- client-ts/src/config/index.ts
+++ client-ts/src/config/index.ts
@@ -1,4 +1,9 @@
-const HOST = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_API_HOSTNAME : process.env.REACT_APP_DEV_API_HOSTNAME;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const HOST = process.env.NODE_ENV === 'production' ? 'https://api.onad.io' : 'http://localhost:3000';
+// const HOST = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_API_HOSTNAME : process.env.REACT_APP_DEV_API_HOSTNAME;
 export const REACT_HOST = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_REACT_HOSTNAME : process.env.REACT_APP_DEV_REACT_HOSTNAME;
 
 export default HOST;
