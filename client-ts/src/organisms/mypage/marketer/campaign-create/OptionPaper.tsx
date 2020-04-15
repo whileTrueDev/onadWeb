d5f85f1f4b3052ca16967683968cc73a80ae8d76
iamsupermazinga <iamsupermazinga@gmail.com>
2020-04-15 21:29:24 +0900

    캠페인 생성 - 옵션선택 - 초안 생성

--- client-ts/src/organisms/mypage/marketer/campaign-create/OptionPaper.tsx
+++ client-ts/src/organisms/mypage/marketer/campaign-create/OptionPaper.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import { Collapse } from '@material-ui/core';
 import OptionSelectPaper from './sub/OptionSelectPaper';
 import CampaignCreateStepLayout from './StepLayout';
 import ButtonSet from './sub/ButtonSet';
-import {
-  Step1Interface,
-  Action
-} from './campaignReducer';
+import AdDescriptionSelect from './sub/AdDescriptionSelect';
+import AdDescriptionDialog from './sub/AdDescriptionDialog';
+import { Step1Interface, Action } from './campaignReducer';
+import useDialog from '../../../../utils/hooks/useDialog';
 
 const options = [
   {
     id: 'option1',
-    primaryText: '배너 광고 + 클릭 광고',
-    secondaryText: '상품, 브랜드 홍보 뿐 아니라 구매 전환까지 하고 싶어요.',
+    primaryText: '온애드 생방송 배너 광고',
+    secondaryText: `실시간 방송 중인 크리에이터를 매칭하여 송출되는 광고로
+    뛰어난 방송화면 내 장시간의 노출로 브랜드 인지 각인 효과가 큽니다.
+    10분마다 광고 전환-롤링 되며, 시청자 노출 수에 기반하여 과금되는 합리적 비용구조를 가지고 있습니다.`,
+    materials: [
+      { name: '생방송 송출 배너', description: '', lastDesc: '' },
+      { name: '클릭 배너', description: '', lastDesc: '' },
+      { name: '챗봇', description: '', lastDesc: '' }
+    ]
   },
   {
     id: 'option0',
-    primaryText: '배너 광고',
-    secondaryText: '상품, 브랜드 홍보만 하고싶어요.',
+    primaryText: '온애드-배너 업로드형 광고',
+    secondaryText: '추가 예정입니다.',
   },
 ];
 
@@ -28,24 +36,23 @@ interface OptionPaperProps {
   handleNext: (event: React.MouseEvent<HTMLButtonElement>) => void;
   step: number;
 }
-
-
 interface OptionInterface {
-  id: string;
-  primaryText: string;
-  secondaryText: string;
+  id: string; primaryText: string; secondaryText: string;
+  materials?: { name: string; description: string; lastDesc: string }[];
 }
-
-
 const OptionPaper = (props: OptionPaperProps): JSX.Element => {
   const {
-    state, dispatch, step, handleNext, handleBack // for '다음' 버튼 관리
+    state, dispatch, step, handleNext, handleBack, // for '다음' 버튼 관리
   } = props;
 
   // option을 선택하였을 때 event listener
   const handleChange = (id: string) => (): void => {
     dispatch({ key: id, value: '' });
   };
+
+  // 광고 유형 선택 - 광고 구성 설명 다이얼로그
+  const adDescriptionDialog = useDialog();
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   return (
     <CampaignCreateStepLayout
@@ -56,15 +63,28 @@ const OptionPaper = (props: OptionPaperProps): JSX.Element => {
       {/* optionType 선택 이전 */}
       {step === 0 && (
         <div>
-          {options.map((opt: OptionInterface) => (
+          {options.map((opt: OptionInterface, index) => (
             <OptionSelectPaper
               key={opt.id}
               primaryText={opt.primaryText}
               secondaryText={opt.secondaryText}
               handleSelect={handleChange(opt.id)}
               checked={state.option === opt.id}
-              disabled={opt.id === 'option0'}
-            />
+              // disabled={opt.id === 'option0'}
+            >
+              {opt.materials && (
+              <Collapse in={state.option === opt.id}>
+                <AdDescriptionSelect
+                  primary={opt.primaryText}
+                  materials={opt.materials}
+                  handleMaterialClick={(): void => {
+                    setSelectedIndex(index);
+                    adDescriptionDialog.handleOpen();
+                  }}
+                />
+              </Collapse>
+              )}
+            </OptionSelectPaper>
           ))}
           <ButtonSet handleNext={handleNext} handleBack={handleBack} set={Boolean(1)} />
         </div>
@@ -86,6 +106,14 @@ const OptionPaper = (props: OptionPaperProps): JSX.Element => {
             ))}
         </div>
       )}
+
+      {adDescriptionDialog.open && options[selectedIndex].materials && (
+      <AdDescriptionDialog
+        open={adDescriptionDialog.open}
+        onClose={adDescriptionDialog.handleClose}
+      />
+      )}
+
     </CampaignCreateStepLayout>
   );
 };
