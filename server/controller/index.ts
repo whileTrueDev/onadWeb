4b8f5bec09a117ee98463262a3cf743c4a977c78
hwasurr <iamsupermazinga@gmail.com>
2020-04-17 13:47:10 +0900

    controller 예시 추가

--- server/controller/index.ts
+++ server/controller/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import createError from 'http-errors';
 import responseMessages from '../lib/responseMessages';
+import { Session } from '../@types/session';
 
 export interface RequestHandler {
   /** handler function (Middleware) */
@@ -14,6 +15,9 @@ export interface RequestPromiseHandler {
 export default class Controller {
   router = express.Router();
 
+  // *********************************
+  // ********** Middlewares **********
+  // *********************************
   protected checkSessionExists: RequestHandler = (req, res, next) => {
     /** ******************
      * Session Check
@@ -46,5 +50,119 @@ export default class Controller {
 
   protected unusedMethod: RequestHandler = (req, res, next) => {
     next(new createError[405](responseMessages.ERROR_405));
+  };
+
+  // *********************************
+  // ************ Helprs *************
+  // *********************************
+  protected getParam = (paramField: string | string[],
+    method: 'get' | 'post' | 'put' | 'patch' | 'delete' | 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+    req: express.Request): any | any[] => {
+    // 파라미터 field가 하나의 문자열인 경우
+    if (typeof paramField === 'string') {
+      switch (method.toLowerCase()) {
+        case 'get':
+          if (!(Object.keys(req.query).includes(paramField))) {
+            throw new createError[400](responseMessages.ERROR_400);
+          }
+          return req.query[paramField];
+        case 'post':
+        case 'put':
+        case 'patch':
+        case 'delete':
+          if (!Object.keys(req.body).includes(paramField)) {
+            throw new createError[400](responseMessages.ERROR_400);
+          }
+          return req.body[paramField];
+        default:
+          throw new Error('getParam에 올바른 Method 명을 입력하지 않았습니다.');
+      }
+    }
+    // 파라미터 field가 배열인 경우
+    switch (method.toLowerCase()) {
+      case 'get':
+        return paramField.map((param) => {
+          if (!(Object.keys(req.query).includes(param))) {
+            throw new createError[400](responseMessages.ERROR_400);
+          }
+          return req.query[param];
+        });
+      case 'post':
+      case 'put':
+      case 'patch':
+      case 'delete':
+        return paramField.map((param) => {
+          if (!Object.keys(req.body).includes(param)) {
+            throw new createError[400](responseMessages.ERROR_400);
+          }
+          return req.body[param];
+        });
+      default:
+        throw new Error('getParam에 올바른 Method 명을 입력하지 않았습니다.');
+    }
+  };
+
+  protected getOptionalParam = (paramField: string | string[],
+    method: 'get' | 'post' | 'put' | 'patch' | 'delete' | 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+    req: express.Request): any | any[] => {
+    // 파라미터 field가 하나의 문자열인 경우
+    if (typeof paramField === 'string') {
+      switch (method.toLowerCase()) {
+        case 'get':
+          return req.query[paramField];
+        case 'post':
+        case 'put':
+        case 'patch':
+        case 'delete':
+          return req.body[paramField];
+        default:
+          throw new Error('getOptionalParam에 올바른 Method 명을 입력하지 않았습니다.');
+      }
+    }
+    // 파라미터 field가 배열인 경우
+    switch (method.toLowerCase()) {
+      case 'get':
+        return paramField.map((param) => req.query[param]);
+      case 'post':
+      case 'put':
+      case 'patch':
+      case 'delete':
+        return paramField.map((param) => req.body[param]);
+      default:
+        throw new Error('getOptionalParam에 올바른 Method 명을 입력하지 않았습니다.');
+    }
+  };
+
+  protected getSessionData = (req: express.Request): Session => {
+    if (req && req.session && req.session.passport && req.session.passport.user) {
+      return req.session.passport.user;
+    }
+    throw new createError[401](responseMessages.ERROR_401);
+  };
+
+  protected send = (
+    resultData: any,
+    method: 'get' | 'post' | 'put' | 'patch' | 'delete' | 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+    res: express.Response
+  ): void => {
+    const OK = 200;
+    const CREATED = 201;
+    switch (method.toLowerCase()) {
+      case 'post':
+        res.status(CREATED).json(resultData);
+        break;
+      case 'get':
+      case 'put':
+      case 'patch':
+      case 'delete':
+        res.status(OK).json(resultData);
+        break;
+      default:
+        throw new Error('send함수에 올바른 Method 명을 입력하지 않았습니다.');
+    }
+  };
+
+  protected promiseError = (error: Error, next: express.NextFunction): void => {
+    next(new createError[500](error.message));
   };
 }
