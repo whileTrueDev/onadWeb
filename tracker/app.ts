1f588493052c1a7b4d22c1577745c35aed3a5105
hwasurr <iamsupermazinga@gmail.com>
2020-04-21 09:42:58 +0900

    tracker adchat, adpanel 라우팅 수정

--- tracker/app.ts
+++ tracker/app.ts
@@ -9,7 +9,6 @@ import path from 'path';
 import morgan from 'morgan';
 import tracking from './lib/tracking';
 import parseUserAgent from './lib/parseUserAgent';
-import CostType from './@types/CostType';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -23,9 +22,34 @@ app.set('view engine', 'ejs');
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common'));
 app.engine('html', require('ejs').renderFile);
 
-app.get('/:costType/:creatorTwitchId', async (req, res, next) => {
+app.get('/:creatorTwitchId', async (req, res, next) => {
+  const costType = 'adpanel';
   // Get semantic parameters
-  const costType = req.params.costType as CostType;
+  const creatorTwitchId = req.params.creatorTwitchId as string;
+  // Get UserAgent and IP
+  const nowIp: string | string[] | undefined = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const uastring = req.headers['user-agent'];
+  const userAgent = parseUserAgent(uastring);
+
+  const result = await tracking(nowIp, userAgent, creatorTwitchId, costType);
+  if (result.href) {
+    res.redirect(301, result.href);
+  } else {
+    let message = '광고주 페이지로 이동중입니다...';
+    if (result.message === 'Invalid ip'
+      || result.message === 'DB Error'
+      || result.message === 'invalid link') {
+      message = '죄송합니다. 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.';
+    } else if (result.message === 'No campaign') {
+      message = `${result.name || creatorTwitchId} 님은 현재 방송중이지 않습니다.`;
+    }
+    res.render('index', { message, twitchlink: `https://twitch.tv/${creatorTwitchId}` });
+  }
+});
+
+app.get('/adchat/:creatorTwitchId', async (req, res, next) => {
+  const costType = 'adchat';
+  // Get semantic parameters
   const creatorTwitchId = req.params.creatorTwitchId as string;
   // Get UserAgent and IP
   const nowIp: string | string[] | undefined = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
