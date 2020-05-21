c077740671fc1f5dc9136916e222765128fc3d10
hwasurr <iamsupermazinga@gmail.com>
2020-05-21 11:47:23 +0900

    테스트 stack 실행 추가

--- aws-cdk/bin/aws-cdk.ts
+++ aws-cdk/bin/aws-cdk.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as dotenv from 'dotenv';
 import * as cdk from '@aws-cdk/core';
 import OnADProductionAwsStack from '../lib/production-stack';
+import OnADTestAwsStack from '../lib/test-stack';
 
 dotenv.config();
 
@@ -10,6 +11,17 @@ const app = new cdk.App();
 ((): OnADProductionAwsStack => new OnADProductionAwsStack(
   app,
   'OnADProductionAwsStack',
+  {
+    env: {
+      account: cdk.Aws.ACCOUNT_ID,
+      region: process.env.AWS_ONAD_REGION
+    }
+  }
+))();
+
+((): OnADTestAwsStack => new OnADTestAwsStack(
+  app,
+  'OnADTestAwsStack',
   {
     env: {
       account: cdk.Aws.ACCOUNT_ID,
