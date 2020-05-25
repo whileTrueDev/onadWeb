3dd576b83af4afe3b9b60e5b205e50b440b71d21
hwasurr <iamsupermazinga@gmail.com>
2020-05-25 17:12:15 +0900

    Add adpick crawl

---
+++ crawl-adpick/src/config/ormconfig.ts
@@ -0,0 +1,21 @@
+import { ConnectionOptions } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: ConnectionOptions = {
+  type: 'mariadb',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  charset: 'utf8mb4',
+  synchronize: true,
+  logging: true,
+  entities: [
+    'src/entity/*.ts', 'modules/**/entity/*.ts', 'src/entity/*.js', 'modules/**/entity/*.js'
+  ],
+};
+
+export default config;
