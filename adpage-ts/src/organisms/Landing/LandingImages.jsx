e9d2107ce3a60bab7dfbbb6e51a3ab661430687b
dn0208@gmail.com <dn0208@gmail.com>
2020-06-03 14:42:43 +0900

    [UPDATE #7.3] 안드로이드 alert 추가 및 이용안내 추가

--- adpage-ts/src/organisms/Landing/LandingImages.jsx
+++ adpage-ts/src/organisms/Landing/LandingImages.jsx
@@ -96,7 +96,7 @@ const useTabValue = () => {
 };
 
 export default function LandingImages(props) {
-  const { campaignData, isDesktopWidth, name } = props;
+  const { campaignData, isDesktopWidth, name, isAndroid } = props;
   const classes = useStyles();
   const { value, handleTabChange } = useTabValue();
   return (
@@ -118,7 +118,7 @@ export default function LandingImages(props) {
       
 
       {/* Image section */}
-      <CPACampaigns campaigns={campaignData.data} isDesktopWidth={isDesktopWidth} name={name}/>
+      <CPACampaigns campaigns={campaignData.data} isDesktopWidth={isDesktopWidth} name={name} isAndroid={isAndroid}/>
     </Grid>
   );
 }
