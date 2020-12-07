3d9d8a40f3a10c959b984bc3b897dc2a2f4d3fd5
hwasurr <iamsupermazinga@gmail.com>
2020-12-07 16:26:28 +0900

    크리에이터 회원가입 - 비밀번호 확인 추가

--- client-ts/src/organisms/main/signup-creator/SignupCreator.tsx
+++ client-ts/src/organisms/main/signup-creator/SignupCreator.tsx
@@ -14,7 +14,7 @@ import HOST from '../../../config';
 import IndentityVerificationDialog from './IdentityVerification';
 import history from '../../../history';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -22,7 +22,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 export interface CreatorSignupInfo {
-  userid: string; passwd: string; pwdVisibility: boolean;
+  userid: string; passwd: string; repasswd: string; pwdVisibility: boolean;
 }
 export default function SignupCreator(): JSX.Element {
   const classes = useStyles();
@@ -30,7 +30,7 @@ export default function SignupCreator(): JSX.Element {
 
   // 회원가입 정보
   const [signupInfo, setSignupInfo] = useState<CreatorSignupInfo>({
-    userid: '', passwd: '', pwdVisibility: false,
+    userid: '', passwd: '', repasswd: '', pwdVisibility: false,
   });
 
   // 에러 정보
@@ -38,6 +38,7 @@ export default function SignupCreator(): JSX.Element {
   const [useridHelperText, setUseridHelperText] = useState(defaultHelperText);
   const [useridError, setUseridError] = useState<boolean>(false);
   const [pwdError, setPwdError] = useState<boolean>(false);
+  const [repwdError, setRePwdError] = useState<boolean>(false);
 
   // 단계 정보
   const [activeStep, setStep] = useState(0);
@@ -51,7 +52,6 @@ export default function SignupCreator(): JSX.Element {
 
   // 본인인증 완료 이후 -> 회원가입 요청
   function handleSignup(): void {
-    console.log(signupInfo.userid, signupInfo.passwd);
     axiosInstance.post(`${HOST}/creator`, {
       userid: signupInfo.userid, passwd: signupInfo.passwd
     })
@@ -65,7 +65,7 @@ export default function SignupCreator(): JSX.Element {
   const handleChange = (
     prop: keyof CreatorSignupInfo
   ) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (prop === 'passwd') {
+    if (prop === 'passwd' || prop === 'repasswd') {
       if (!signupInfo.pwdVisibility) {
         setSignupInfo({ ...signupInfo, [prop]: event.target.value });
       }
@@ -102,11 +102,14 @@ export default function SignupCreator(): JSX.Element {
     return true;
   }
   // 아이디 중복 체크
+  const [duplicateCheckLoading, setLoading] = useState<boolean>(false);
   async function duplicateCheck(): Promise<boolean> {
+    setLoading(true);
     const result = await axiosInstance.get<'duplicate' | 'allow'>(
       `${HOST}/creator/check-id`, { params: { userid: signupInfo.userid } }
     )
       .then((res) => {
+        setLoading(false);
         if (res.data === 'duplicate') {
           setUseridHelperText('중복되는 아이디가 존재합니다.');
           setUseridError(true);
@@ -116,6 +119,7 @@ export default function SignupCreator(): JSX.Element {
         return false;
       })
       .catch(() => {
+        setLoading(false);
         setUseridHelperText('아이디 중복확인 도중에 오류가 발생했습니다. support@onad.io로 문의바랍니다.');
         setUseridError(true);
         return false;
@@ -129,8 +133,14 @@ export default function SignupCreator(): JSX.Element {
     const isValid = regx.test(signupInfo.passwd);
     if (!isValid) {
       setPwdError(true);
+      return false;
     }
-    return isValid;
+    // 비번 + 비번 확인 동일한지 체크
+    if (signupInfo.passwd !== signupInfo.repasswd) {
+      setRePwdError(true);
+      return false;
+    }
+    return true;
   }
 
   // form 작성 오류 검사 -> 통과시 본인인증 다이얼로그 오픈
@@ -151,6 +161,7 @@ export default function SignupCreator(): JSX.Element {
   // input icon 색상
   const [useridIconColor, setUseridIconColor] = useState<'primary' | 'disabled'>('disabled');
   const [pwIconColor, setPwIconColor] = useState<'primary' | 'disabled'>('disabled');
+  const [rePwIconColor, setRePwIconColor] = useState<'primary' | 'disabled'>('disabled');
 
   return (
     <div className={classes.root}>
@@ -175,10 +186,12 @@ export default function SignupCreator(): JSX.Element {
                 onSubmit={handleSubmit}
                 style={{ marginTop: 16, marginBottom: 16, minWidth: 270 }}
               >
+                {/* 아이디 */}
                 <TextField
                   style={{ marginBottom: 16 }}
                   fullWidth
                   variant="outlined"
+                  disabled={duplicateCheckLoading}
                   placeholder="아이디"
                   onChange={handleChange('userid')}
                   value={signupInfo.userid}
@@ -194,11 +207,13 @@ export default function SignupCreator(): JSX.Element {
                     )
                   }}
                 />
+                {/* 비밀번호 */}
                 <TextField
                   style={{ marginBottom: 16 }}
                   fullWidth
                   variant="outlined"
                   placeholder="비밀번호"
+                  disabled={duplicateCheckLoading}
                   type={signupInfo.pwdVisibility ? 'text' : 'password'}
                   error={pwdError}
                   helperText="특수문자를 포함한 8-20자 영문 또는 숫자"
@@ -225,6 +240,39 @@ export default function SignupCreator(): JSX.Element {
                     )
                   }}
                 />
+                {/* 비밀번호 확인 */}
+                <TextField
+                  style={{ marginBottom: 16 }}
+                  fullWidth
+                  variant="outlined"
+                  placeholder="비밀번호확인"
+                  disabled={duplicateCheckLoading}
+                  type={signupInfo.pwdVisibility ? 'text' : 'password'}
+                  error={repwdError}
+                  helperText={repwdError ? '비밀번호와 동일하지 않습니다.' : ''}
+                  value={signupInfo.repasswd}
+                  onChange={handleChange('repasswd')}
+                  onFocus={(): void => setRePwIconColor('primary')}
+                  onBlur={(): void => setRePwIconColor('disabled')}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Lock color={rePwIconColor} />
+                      </InputAdornment>
+                    ),
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                        >
+                          {signupInfo.pwdVisibility ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
 
                 <Button
                   type="submit"
@@ -232,7 +280,7 @@ export default function SignupCreator(): JSX.Element {
                   size="large"
                   variant="contained"
                   style={{ width: '100%' }}
-                  disabled={!signupInfo.userid || !signupInfo.passwd}
+                  disabled={!signupInfo.userid || !signupInfo.passwd || !signupInfo.repasswd}
                 >
                   가입하기
                 </Button>
@@ -250,7 +298,6 @@ export default function SignupCreator(): JSX.Element {
               onBackClick={handleBack}
             />
             )}
-
 
             {!(window.location.pathname === '/creator/signup/complete') && (
               <>
