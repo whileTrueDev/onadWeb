6c6bc10bc36ebf9894a6dc368fd4fcbadc5fd5d1
hwasurr <iamsupermazinga@gmail.com>
2021-04-08 14:34:23 +0900

    Fix: 캠페인 slack알림 off -> on

--- server/routes/marketer/campaign/index.ts
+++ server/routes/marketer/campaign/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import slack from '../../../lib/slack/messageWithJson';
 import responseHelper from '../../../middlewares/responseHelper';
 import doQuery from '../../../model/doQuery';
 import dataProcessing from '../../../lib/dataProcessing';
@@ -269,14 +270,14 @@ router.route('/')
               marketerActionLogging([
                 marketerId, MARKETER_ACTION_LOG_TYPE, JSON.stringify({ campaignName })
               ]);
-              // slack({
-              //   summary: '캠페인 등록 알림',
-              //   text: '관리자 페이지에서 방금 등록된 캠페인을 확인하세요.',
-              //   fields: [
-              //     { title: '마케터 이름', value: marketerName!, short: true },
-              //     { title: '캠페인 이름', value: campaignName!, short: true },
-              //   ]
-              // });
+              slack({
+                summary: '캠페인 등록 알림',
+                text: '관리자 페이지에서 방금 등록된 캠페인을 확인하세요.',
+                fields: [
+                  { title: '마케터 이름', value: marketerName!, short: true },
+                  { title: '캠페인 이름', value: campaignName!, short: true },
+                ]
+              });
             })
             .catch((error) => {
               responseHelper.promiseError(error, next);
