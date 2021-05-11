b9a7dbe52578fd0586948b8a37a6f39ba9092bd9
hwasurr <iamsupermazinga@gmail.com>
2021-05-11 15:40:07 +0900

    Add: CPS 계산 배송비 처리 추가

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,16 @@
 // 환경변수를 위해. dev환경: .env 파일 / production환경: docker run의 --env-file인자로 넘김.
 require('dotenv').config();
 
-require('./javascripts/calculation_v.4')()
-  // CPC계산
-  .then(require('./javascripts/trackingCalculator_v.3'))
-  // 추천인 코드 계산
-  .then(require('./javascripts/referralCodeCalculator'))
-  // CPS 계산
-  .then(require('./javascripts/cpsCalculator'))
-  .then(() => {
-    process.exit(0);
-  });
+// require('./javascripts/calculation_v.4')()
+//   // CPC계산
+//   .then(require('./javascripts/trackingCalculator_v.3'))
+//   // 추천인 코드 계산
+//   .then(require('./javascripts/referralCodeCalculator'))
+//   // CPS 계산
+//   .then(require('./javascripts/cpsCalculator'))
+//   .then(() => {
+//     process.exit(0);
+//   });
+
+
+require('./javascripts/cpsCalculator')();
