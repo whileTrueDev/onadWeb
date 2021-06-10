22ef6745267de91eedcef8be1dc41f7c12fac5b9
hwasurr <iamsupermazinga@gmail.com>
2021-06-10 17:36:54 +0900

    Add: 크리에이터 twitch oauth 연동 -> req.user 없는 오류 수정

--- api/src/resources/auth/guards/preCreatorTwitch.guard.ts
+++ api/src/resources/auth/guards/preCreatorTwitch.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class PreCreatorTwitchAuthGuard extends AuthGuard('twitch-pre-creator') {}
+export class PreCreatorTwitchAuthGuard extends AuthGuard('twitch-pre-creator') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return result;
+  }
+}
--- api/src/resources/auth/guards/twitchLink.guard.ts
+++ api/src/resources/auth/guards/twitchLink.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class TwitchLinkGuard extends AuthGuard('twitch-link') {}
+export class TwitchLinkGuard extends AuthGuard('twitch-link') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return result;
+  }
+}
