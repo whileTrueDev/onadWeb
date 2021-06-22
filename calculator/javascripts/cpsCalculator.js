b1dd9fa79be040ee463808b3cdcd119287fe3d68
hwasurr <iamsupermazinga@gmail.com>
2021-06-22 15:12:18 +0900

    Update: 라이브커머스/CPS 계산 round -> floor

--- calculator/javascripts/cpsCalculator.js
+++ calculator/javascripts/cpsCalculator.js
@@ -2,7 +2,10 @@
 const doQuery = require('../model/calculatorQuery');
 const pool = require('../model/connectionPool');
 const {
-  getUpdateFlag, getInsertCampaignLog, getCalculateCreatorIncome, getCalculateMarketerSalesIncome
+  getUpdateFlag,
+  getInsertCampaignLog,
+  getCalculateCreatorIncome,
+  getCalculateMarketerSalesIncome,
 } = require('../utils/cps/queries');
 const { getFeeCalculatedTargets } = require('../utils/cps/commission');
 const generateOptimizedDataSet = require('../utils/cps/generateOptimizedDataSet');
@@ -36,7 +39,7 @@ const getTargets = async () => {
   WHERE statusString = ? AND calculateDoneFlag = ? AND isLiveCommerce = ?`;
 
   const { result } = await doQuery(query, ['구매확정', false, false]) // 실 구동시, "구매확정"
-    .catch((err) => `error occurred during run getTargets - ${err}`);
+    .catch(err => `error occurred during run getTargets - ${err}`);
 
   return result;
 };
@@ -56,35 +59,43 @@ const getLiveCommerceTargets = async () => {
   WHERE statusString = ? AND calculateDoneFlag = ? AND isLiveCommerce = ?`;
 
   const { result } = await doQuery(query, ['구매확정', false, true]) // 실 구동시, "구매확정"
-    .catch((err) => `error occurred during run getTargets - ${err}`);
+    .catch(err => `error occurred during run getTargets - ${err}`);
 
-  return result.map((res) => ({
+  return result.map(res => ({
     ...res,
     targetCreatorId: JSON.parse(res.targetList).targetList[0], // 라이브커머스는 언제나 1명. (2명이상일 시 무시)
   }));
 };
-
 
 /**
  * CPS 캠페인 계산 작업을 실행합니다.
  * 1. 캠페인 계산 로그 처리 (campaignLog - campaignId, creatorId, type=CPS, cashToCreator, salesIncomeToMarketer)
  * 2. 광고주 판매대금 처리 (marketerSalesIncome - marketerId, 추가금액)
  */
-async function calculate(conn, {
-  marketerId, cashToCreators, salesIncomeToMarketer, deliveryFee
-}) {
+async function calculate(conn, { marketerId, cashToCreators, salesIncomeToMarketer, deliveryFee }) {
   // * 1. 광고주 판매대금 처리 (marketerSalesIncome - marketerId, 추가금액)
-  const salesIncome = getCalculateMarketerSalesIncome({ marketerId, salesIncomeToMarketer, deliveryFee });
+  const salesIncome = getCalculateMarketerSalesIncome({
+    marketerId,
+    salesIncomeToMarketer,
+    deliveryFee,
+  });
   await conn.query(salesIncome.query, salesIncome.queryArray);
 
   // * 2. 방송인 수익금 처리 (creatorIncome - creatorId, 추가금액)
-  cashToCreators.forEach(async ({ amount, creatorId }) => {
-    const creatorIncome = getCalculateCreatorIncome({ creatorId, cashToCreator: amount });
-    if (creatorIncome) {
-      await conn.query(creatorIncome.query, creatorIncome.queryArray);
-    }
-  });
-  console.log(`[${new Date().toLocaleString()}] 광고주${marketerId} (방송인: ${cashToCreators.map((x) => x.creatorId)}) 계산 완료`);
+  await Promise.all(
+    cashToCreators.map(async ({ amount, creatorId }) => {
+      const creatorIncome = getCalculateCreatorIncome({ creatorId, cashToCreator: amount });
+      if (creatorIncome) {
+        return conn.query(creatorIncome.query, creatorIncome.queryArray);
+      }
+      return null;
+    }),
+  );
+  console.log(
+    `[${new Date().toLocaleString()}] 광고주${marketerId} (방송인: ${cashToCreators.map(
+      x => x.creatorId,
+    )}) 계산 완료`,
+  );
 }
 
 // * 3. 모두 완료 후, 주문의 계산완료플래그를 true로 처리 (merchandiseOrders - calculateDoneFlag)
@@ -103,27 +114,31 @@ const updateCampaignLog = (conn, feeCalculatedTargets) => {
 /**
  * ### CPS 계산 정책
  * 1. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기고** 구매완료 시
- *    - 기본 ⇒ 광고주 70 %, 방송인 A 20 %, 온애드 10 %  
+ *    - 기본 ⇒ 광고주 70 %, 방송인 A 20 %, 온애드 10 %
  *    (`merchandiseRegistered.creatorCommission`와 `merchandiseRegistered.onadCommission` 값에 의해 달라질 수 있다.)
- * 
+ *
  * 2. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기지 않고** 구매완료 시
- *    - 기본 **⇒ 광고주 90 %, 온애드 10 %**  
+ *    - 기본 **⇒ 광고주 90 %, 온애드 10 %**
  *    (`merchandiseRegistered.onadCommission` 값에 의해 달라질 수 있다.)
  */
 async function cpsCalculate() {
   console.info('[CPS|라이브커머스] 판매 대금 계산을 시작합니다.');
 
-  const cpsTargets = await getTargets()
-    .catch((err) => console.error(`CPS 계산 대상 목록 가져오는 도중 오류 - ${err}`));
-  const liveCommeerceTargets = await getLiveCommerceTargets()
-    .catch((err) => console.error(`[liveCommerce] 계산 대상 목록 가져오는 도중 오류 - ${err}`));
+  const cpsTargets = await getTargets().catch(err =>
+    console.error(`CPS 계산 대상 목록 가져오는 도중 오류 - ${err}`),
+  );
+  const liveCommeerceTargets = await getLiveCommerceTargets().catch(err =>
+    console.error(`[liveCommerce] 계산 대상 목록 가져오는 도중 오류 - ${err}`),
+  );
 
   const targets = cpsTargets.concat(liveCommeerceTargets);
   const feeCalculatedTargets = getFeeCalculatedTargets(targets);
   console.info('[CPS|라이브커머스] 판매 대금 계산 타겟 수: ', targets.length);
 
   const calcTargets = generateOptimizedDataSet(feeCalculatedTargets);
-  calcTargets.forEach((t) => { console.log(t); });
+  calcTargets.forEach(t => {
+    console.log(t);
+  });
 
   const conn = await pool.promise().getConnection();
   try {
@@ -137,21 +152,27 @@ async function cpsCalculate() {
 
     // * 1. 광고주 판매대금 처리 (marketerSalesIncome - marketerId, 추가금액)
     // * 2. 방송인 수익금 처리 (creatorIncome - creatorId, 추가금액)
-    await Promise.all(calcTargets.map((target) => calculate(conn, target)));
+    await Promise.all(calcTargets.map(target => calculate(conn, target)));
 
     // * 3. 모두 완료 후, 주문의 계산완료플래그를 true로 처리
-    await Promise.all(targets.map((t) => updateCalculateDoneFlag(conn, t.id)))
-      .then(() => console.log(`[${new Date().toLocaleString()}] 모든 주문의 계산완료플래그를 true로 처리 완료`));
+    await Promise.all(targets.map(t => updateCalculateDoneFlag(conn, t.id))).then(() =>
+      console.log(`[${new Date().toLocaleString()}] 모든 주문의 계산완료플래그를 true로 처리 완료`),
+    );
 
     // * 4. 캠페인 계산 로그 처리
-    await updateCampaignLog(conn, feeCalculatedTargets)
-      .then(() => console.log(`[${new Date().toLocaleString()}] 모든 주문의 캠페인 계산 로그 처리 완료`));
+    await updateCampaignLog(conn, feeCalculatedTargets).then(() =>
+      console.log(`[${new Date().toLocaleString()}] 모든 주문의 캠페인 계산 로그 처리 완료`),
+    );
 
-    console.log(`[${new Date().toLocaleString()}] [CPS|라이브커머스] 판매 대금 계산을 모두 완료하였습니다.`);
+    console.log(
+      `[${new Date().toLocaleString()}] [CPS|라이브커머스] 판매 대금 계산을 모두 완료하였습니다.`,
+    );
     return conn.commit();
   } catch (e) {
     await conn.rollback();
-    console.error(`[${new Date().toLocaleString()}] [CPS|라이브커머스] 판매 대금 계산 중 오류 발생`);
+    console.error(
+      `[${new Date().toLocaleString()}] [CPS|라이브커머스] 판매 대금 계산 중 오류 발생`,
+    );
     console.error(e);
     throw e;
   } finally {
--- calculator/utils/cps/commission.js
+++ calculator/utils/cps/commission.js
@@ -1,41 +1,45 @@
 /**
-   * 각 유저별 계산 대금을 구합니다. 리뷰/자랑하기/응원하기 글의 대상인 방송인이 있는 지 여부를 기준으로
-   * 계산 대금은 다르게 계산됩니다.
-   * @author hwasurr
-   * @param { number } orderPrice 계산이 필요한 판매 대금
-   * @param { boolean } isCreatorExists 리뷰/자랑하기/응원하기 글의 대상인 방송인이 있는지 여부
-   * 1. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기고** 구매완료 시
-   *    - **⇒ 광고주 70 %, 방송인 A 20 %, 온애드 10 %**
-   * 
-   * 2. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기지 않고** 구매완료 시
-   *    - **⇒ 광고주 90 %, 온애드 10 %**
-   */
+ * 각 유저별 계산 대금을 구합니다. 리뷰/자랑하기/응원하기 글의 대상인 방송인이 있는 지 여부를 기준으로
+ * 계산 대금은 다르게 계산됩니다.
+ * @author hwasurr
+ * @param { number } orderPrice 계산이 필요한 판매 대금
+ * @param { boolean } isCreatorExists 리뷰/자랑하기/응원하기 글의 대상인 방송인이 있는지 여부
+ * 1. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기고** 구매완료 시
+ *    - **⇒ 광고주 70 %, 방송인 A 20 %, 온애드 10 %**
+ *
+ * 2. 방송인 A로부터 유입된 시청자가 구입 + 인증(리뷰or자랑하기)**글을 남기지 않고** 구매완료 시
+ *    - **⇒ 광고주 90 %, 온애드 10 %**
+ */
 const getCashesCalculated = ({
-  totalOrderPrice, isCreatorExists, creatorCommission, onadCommission
+  totalOrderPrice,
+  isCreatorExists,
+  creatorCommission,
+  onadCommission,
 }) => {
-  if (isCreatorExists) { // 1 번의 경우
-    const cashToCreator = Math.round(totalOrderPrice * creatorCommission);
-    const cashToOnad = Math.round(totalOrderPrice * onadCommission);
+  if (isCreatorExists) {
+    // 1 번의 경우
+    const cashToCreator = Math.floor(totalOrderPrice * creatorCommission);
+    const cashToOnad = Math.floor(totalOrderPrice * onadCommission);
     const salesIncomeToMarketer = totalOrderPrice - cashToCreator - cashToOnad;
     return { cashToCreator, salesIncomeToMarketer };
   }
   const cashToCreator = 0;
-  const cashToOnad = Math.round(totalOrderPrice * onadCommission);
+  const cashToOnad = Math.floor(totalOrderPrice * onadCommission);
   const salesIncomeToMarketer = totalOrderPrice - cashToOnad;
   return { cashToCreator, salesIncomeToMarketer };
 };
 
 // CPS 계산 타겟들의 수수료 계산
-const getFeeCalculatedTargets = (_targets) => _targets.map((target) => {
-  const { cashToCreator, salesIncomeToMarketer } = getCashesCalculated({
-    totalOrderPrice: target.orderPrice,
-    isCreatorExists: !!target.targetCreatorId,
-    creatorCommission: target.creatorCommission,
-    onadCommission: target.onadCommission,
+const getFeeCalculatedTargets = _targets =>
+  _targets.map(target => {
+    const { cashToCreator, salesIncomeToMarketer } = getCashesCalculated({
+      totalOrderPrice: target.orderPrice,
+      isCreatorExists: !!target.targetCreatorId,
+      creatorCommission: target.creatorCommission,
+      onadCommission: target.onadCommission,
+    });
+    return { ...target, cashToCreator, salesIncomeToMarketer };
   });
-  return { ...target, cashToCreator, salesIncomeToMarketer };
-});
-
 
 module.exports = {
   getCashesCalculated,
